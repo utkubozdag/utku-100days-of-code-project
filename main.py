00da65b67d0a1b4e43a0b96afda7cf1d070436dd
Utku Bozdag <utkubozdag1@gmail.com>
2023-01-24 16:36:08 +0100

    7th commit

--- main.py
+++ main.py
@@ -21,12 +21,12 @@ from dotenv import load_dotenv
 Base = declarative_base()
 load_dotenv(r'C:\Users\utkub\Downloads\.env')
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get['SECRET_KEY']
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
